d93f9b9ad2718933fd60f56ad1b3c21502c0cf2a
kakaict <canhcung88@gmail.com>
2018-05-23 20:38:08 +0200

    update

---
+++ src/main/java/builder/message/svn/GitBuilder.java
@@ -0,0 +1,16 @@
+package builder.message.svn;
+
+/**
+ * Created by khaitq
+ * Date: 23/05/2018
+ * Github:  https://github.com/quangkhai88
+ */
+
+public class GitBuilder implements Buildable {
+
+    @Override
+    public Object build(String input) {
+
+        return null;
+    }
+}
