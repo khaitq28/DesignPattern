196976a0a7f5fd31e92b74fcc435faf4254e9bc3
TRAN Quang Khai <canhcung88@gmail.com>
2020-03-22 00:39:41 +0100

    update

--- src/main/java/objectBuilder/Client.java
+++ src/main/java/objectBuilder/Client.java
@@ -1,5 +1,7 @@
 package objectBuilder;
 
+import objectBuilder.more.AmazonException;
+
 public class Client {
 
     public static  void main(String[] args) {
@@ -13,15 +15,13 @@ public class Client {
 
         String var = "ABCD";
         NotFoundException ex = new NotFoundException.Builder().addMessage(var).setHttpCode(400).build();
-
         ex.print();
 
-
         Entity entity = new Entity.Builder().setMarque("mar").setModel("mod").setName("bmw").build();
-
         entity.print();
 
-
+        AmazonException amazonException = new AmazonException.Builder().addMessage("OK").setHttpCode(120).build();
+        amazonException.print();
     }
 
 
