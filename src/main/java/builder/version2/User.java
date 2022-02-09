cb6726b57d0aeb2b5da2125b72cbe353e0debe2f
quangkhai88 <canhcung88@gmail.com>
2022-02-09 13:52:23 +0100

    update

---
+++ src/main/java/builder/version2/User.java
@@ -0,0 +1,34 @@
+package builder.version2;
+
+public class User {
+
+    private final int id;
+
+    private final String name;
+
+    private User (UserBuilder userBuilder) {
+        this.id = userBuilder.id;
+        this.name = userBuilder.name;
+    }
+
+    public static class UserBuilder {
+
+        private int id;
+        private String name;
+
+        public UserBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public  UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public User build() {
+            User user = new User(this);
+            return user;
+        }
+    }
+}
